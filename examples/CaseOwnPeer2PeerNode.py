fc9b170c8199336ccf5bbaff73014e9187b56f76
Nick Vovk <dyvniy@yandex.ru>
2024-03-11 18:29:57 +0200

    better example with input wait

---
+++ examples/CaseOwnPeer2PeerNode.py
@@ -0,0 +1,67 @@
+#######################################################################################################################
+# Author: Nick Vovk                                                                                             #
+# Version: 0.2 beta (use at your own risk)                                                                            #
+#                                                                                                                     #
+# CaseOwnPeer2PeerNode is an example how to switch incoming requests        #
+# 10/03/2024: created
+#######################################################################################################################
+from p2pnetwork.node import Node
+from MyOwnPeer2PeerNode import MyOwnPeer2PeerNode
+import pickle
+import zlib
+
+class CaseOwnPeer2PeerNode(MyOwnPeer2PeerNode):
+
+    # Python class constructor
+    def __init__(self, host, port, id=None, callback=None, max_connections=100):
+        super(CaseOwnPeer2PeerNode, self).__init__(host, port, id, callback, max_connections)
+        print("MyPeer2PeerNode: Started")
+
+    # all the methods below are called when things happen in the network.
+    # implement your network node behavior to create the required functionality.
+
+    def outbound_node_connected(self, node):
+        # super(CaseOwnPeer2PeerNode, self).outbound_node_connected(node)
+        pass
+        
+    def inbound_node_connected(self, node):
+        # super(CaseOwnPeer2PeerNode, self).inbound_node_connected(node)
+        pass
+        
+    def inbound_node_disconnected(self, node):
+        # super(CaseOwnPeer2PeerNode, self).inbound_node_disconnected(node)
+        pass
+        
+    def outbound_node_disconnected(self, node):
+        # super(CaseOwnPeer2PeerNode, self).outbound_node_disconnected(node)
+        pass
+        
+    def node_disconnect_with_outbound_node(self, node):
+        # super(CaseOwnPeer2PeerNode, self).node_disconnect_with_outbound_node(node)
+        pass
+        
+    def node_request_to_stop(self):
+        # super(CaseOwnPeer2PeerNode, self).node_request_to_stop()
+        pass
+
+    def node_message(self, node, data):
+        super(CaseOwnPeer2PeerNode, self).node_message(node, data)
+        # self.id + " - " + node.id + ": " + str(data)
+        if 'list' in data:
+            self.send_list(data['list'])
+
+    def send_list(self, params):
+        print('- send list ' + str(params))
+        # for node in self.all_nodes:
+        #    print(node.host, node.port) 
+        # self.print_connections()
+        nodes = self.all_nodes
+        nodelist = []
+        for node in nodes:
+            nodelist.append((node.host, node.port))
+        # du = pickle.dumps(nodelist)
+        # zi = zlib.compress(du)
+        # self.send_to_nodes({"pickled_zipped": zi})
+        self.send_to_nodes({"hello": nodelist})
+
+
---
+++ examples/my_own_p2p_application_make_list.py
@@ -0,0 +1,64 @@
+#######################################################################################################################
+# Author: Nick Vovk                                                                                         #
+# Version: 0.1 beta (use at your own risk)                                                                            #
+#                                                                                                                     #
+# This example show how to derive a own Node class (CaseOwnPeer2PeerNode) from p2pnet.Node to implement your own Node   #
+# implementation. See the CaseOwnPeer2PeerNode.py for all the details. In that class all your own application specific  #
+# details are coded.                                                                                                  #
+#######################################################################################################################
+
+import sys
+import time
+sys.path.insert(0, '..') # Import the files where the modules are located
+
+from CaseOwnPeer2PeerNode import CaseOwnPeer2PeerNode
+
+node_1 = CaseOwnPeer2PeerNode("127.0.0.1", 8001, 'first_1')
+node_2 = CaseOwnPeer2PeerNode("127.0.0.1", 8002, 'secnd_2')
+node_3 = CaseOwnPeer2PeerNode("192.168.88.193", 8003, 'third_3')
+
+time.sleep(1)
+
+node_1.start()
+node_2.start()
+node_3.start()
+
+time.sleep(1)
+
+debug = False
+node_1.debug = debug
+node_2.debug = debug
+node_3.debug = debug
+
+
+node_1.connect_with_node('127.0.0.1', 8002)
+node_2.connect_with_node('192.168.88.193', 8003)
+node_3.connect_with_node('127.0.0.1', 8001)
+
+time.sleep(2)
+
+node_1.send_to_nodes("message: Hi there!")
+node_2.send_to_nodes({ "name" : "Maurice", "number" : 11, "list": 0 })
+
+time.sleep(5)
+
+print("press enter to stop")
+input()
+print("node 1 is stopping..")
+node_1.stop()
+
+time.sleep(10)
+
+node_2.send_to_nodes("message: Hi there node 2!")
+node_2.send_to_nodes("message: Hi there node 2!")
+node_2.send_to_nodes("message: Hi there node 2!")
+node_3.send_to_nodes("message: Hi there node 2!")
+node_3.send_to_nodes("message: Hi there node 2!")
+node_3.send_to_nodes("message: Hi there node 2!")
+
+time.sleep(5)
+
+node_1.stop()
+node_2.stop()
+node_3.stop()
+print('end test')
